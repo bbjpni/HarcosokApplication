65caecf1fc49dd8ad6ee803d4543c6795cdcd891
Zsombor <brozsek.zsombor@petrik.hu>
2020-12-08 15:04:49 +0100

    Add project files.

---
+++ HarcosokApplication/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace HarcosokApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GBharcosLetrehozas = new System.Windows.Forms.GroupBox();
+            this.BTNharcosLetrehozas = new System.Windows.Forms.Button();
+            this.TBharcosNev = new System.Windows.Forms.TextBox();
+            this.LBnev = new System.Windows.Forms.Label();
+            this.GBkepessegHozzaadas = new System.Windows.Forms.GroupBox();
+            this.TBleiras = new System.Windows.Forms.TextBox();
+            this.LBleiras = new System.Windows.Forms.Label();
+            this.LBhasznalo = new System.Windows.Forms.Label();
+            this.CBhasznalo = new System.Windows.Forms.ComboBox();
+            this.BTNkepesseg = new System.Windows.Forms.Button();
+            this.TBnev2 = new System.Windows.Forms.TextBox();
+            this.LBnev2 = new System.Windows.Forms.Label();
+            this.LBharcosok = new System.Windows.Forms.Label();
+            this.LBOXharcosok = new System.Windows.Forms.ListBox();
+            this.LBOXkepessegek = new System.Windows.Forms.ListBox();
+            this.LBkepessegek = new System.Windows.Forms.Label();
+            this.BTNtöröl = new System.Windows.Forms.Button();
+            this.TBleiras2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BTNmodositas = new System.Windows.Forms.Button();
+            this.GBharcosLetrehozas.SuspendLayout();
+            this.GBkepessegHozzaadas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GBharcosLetrehozas
+            // 
+            this.GBharcosLetrehozas.Controls.Add(this.BTNharcosLetrehozas);
+            this.GBharcosLetrehozas.Controls.Add(this.TBharcosNev);
+            this.GBharcosLetrehozas.Controls.Add(this.LBnev);
+            this.GBharcosLetrehozas.Location = new System.Drawing.Point(13, 13);
+            this.GBharcosLetrehozas.Name = "GBharcosLetrehozas";
+            this.GBharcosLetrehozas.Size = new System.Drawing.Size(775, 64);
+            this.GBharcosLetrehozas.TabIndex = 0;
+            this.GBharcosLetrehozas.TabStop = false;
+            this.GBharcosLetrehozas.Text = "Harcos létrehozása";
+            // 
+            // BTNharcosLetrehozas
+            // 
+            this.BTNharcosLetrehozas.Location = new System.Drawing.Point(181, 28);
+            this.BTNharcosLetrehozas.Name = "BTNharcosLetrehozas";
+            this.BTNharcosLetrehozas.Size = new System.Drawing.Size(105, 22);
+            this.BTNharcosLetrehozas.TabIndex = 2;
+            this.BTNharcosLetrehozas.Text = "Létrehozás";
+            this.BTNharcosLetrehozas.UseVisualStyleBackColor = true;
+            // 
+            // TBharcosNev
+            // 
+            this.TBharcosNev.Location = new System.Drawing.Point(49, 28);
+            this.TBharcosNev.Name = "TBharcosNev";
+            this.TBharcosNev.Size = new System.Drawing.Size(126, 22);
+            this.TBharcosNev.TabIndex = 1;
+            // 
+            // LBnev
+            // 
+            this.LBnev.AutoSize = true;
+            this.LBnev.Location = new System.Drawing.Point(6, 28);
+            this.LBnev.Name = "LBnev";
+            this.LBnev.Size = new System.Drawing.Size(37, 17);
+            this.LBnev.TabIndex = 0;
+            this.LBnev.Text = "Név:";
+            // 
+            // GBkepessegHozzaadas
+            // 
+            this.GBkepessegHozzaadas.Controls.Add(this.TBleiras);
+            this.GBkepessegHozzaadas.Controls.Add(this.LBleiras);
+            this.GBkepessegHozzaadas.Controls.Add(this.LBhasznalo);
+            this.GBkepessegHozzaadas.Controls.Add(this.CBhasznalo);
+            this.GBkepessegHozzaadas.Controls.Add(this.BTNkepesseg);
+            this.GBkepessegHozzaadas.Controls.Add(this.TBnev2);
+            this.GBkepessegHozzaadas.Controls.Add(this.LBnev2);
+            this.GBkepessegHozzaadas.Location = new System.Drawing.Point(13, 83);
+            this.GBkepessegHozzaadas.Name = "GBkepessegHozzaadas";
+            this.GBkepessegHozzaadas.Size = new System.Drawing.Size(775, 137);
+            this.GBkepessegHozzaadas.TabIndex = 3;
+            this.GBkepessegHozzaadas.TabStop = false;
+            this.GBkepessegHozzaadas.Text = "Képesség hozzáadása";
+            // 
+            // TBleiras
+            // 
+            this.TBleiras.Location = new System.Drawing.Point(263, 38);
+            this.TBleiras.Multiline = true;
+            this.TBleiras.Name = "TBleiras";
+            this.TBleiras.Size = new System.Drawing.Size(494, 52);
+            this.TBleiras.TabIndex = 7;
+            // 
+            // LBleiras
+            // 
+            this.LBleiras.AutoSize = true;
+            this.LBleiras.Location = new System.Drawing.Point(706, 18);
+            this.LBleiras.Name = "LBleiras";
+            this.LBleiras.Size = new System.Drawing.Size(51, 17);
+            this.LBleiras.TabIndex = 6;
+            this.LBleiras.Text = "Leírás:";
+            // 
+            // LBhasznalo
+            // 
+            this.LBhasznalo.AutoSize = true;
+            this.LBhasznalo.Location = new System.Drawing.Point(13, 37);
+            this.LBhasznalo.Name = "LBhasznalo";
+            this.LBhasznalo.Size = new System.Drawing.Size(71, 17);
+            this.LBhasznalo.TabIndex = 5;
+            this.LBhasznalo.Text = "Használó:";
+            // 
+            // CBhasznalo
+            // 
+            this.CBhasznalo.FormattingEnabled = true;
+            this.CBhasznalo.Location = new System.Drawing.Point(90, 34);
+            this.CBhasznalo.Name = "CBhasznalo";
+            this.CBhasznalo.Size = new System.Drawing.Size(167, 24);
+            this.CBhasznalo.TabIndex = 4;
+            // 
+            // BTNkepesseg
+            // 
+            this.BTNkepesseg.Location = new System.Drawing.Point(9, 101);
+            this.BTNkepesseg.Name = "BTNkepesseg";
+            this.BTNkepesseg.Size = new System.Drawing.Size(105, 30);
+            this.BTNkepesseg.TabIndex = 2;
+            this.BTNkepesseg.Text = "Hozzáad";
+            this.BTNkepesseg.UseVisualStyleBackColor = true;
+            // 
+            // TBnev2
+            // 
+            this.TBnev2.Location = new System.Drawing.Point(90, 68);
+            this.TBnev2.Name = "TBnev2";
+            this.TBnev2.Size = new System.Drawing.Size(167, 22);
+            this.TBnev2.TabIndex = 1;
+            // 
+            // LBnev2
+            // 
+            this.LBnev2.AutoSize = true;
+            this.LBnev2.Location = new System.Drawing.Point(13, 71);
+            this.LBnev2.Name = "LBnev2";
+            this.LBnev2.Size = new System.Drawing.Size(37, 17);
+            this.LBnev2.TabIndex = 0;
+            this.LBnev2.Text = "Név:";
+            // 
+            // LBharcosok
+            // 
+            this.LBharcosok.AutoSize = true;
+            this.LBharcosok.Location = new System.Drawing.Point(26, 232);
+            this.LBharcosok.Name = "LBharcosok";
+            this.LBharcosok.Size = new System.Drawing.Size(68, 17);
+            this.LBharcosok.TabIndex = 8;
+            this.LBharcosok.Text = "Harcosok";
+            // 
+            // LBOXharcosok
+            // 
+            this.LBOXharcosok.FormattingEnabled = true;
+            this.LBOXharcosok.ItemHeight = 16;
+            this.LBOXharcosok.Location = new System.Drawing.Point(22, 253);
+            this.LBOXharcosok.Name = "LBOXharcosok";
+            this.LBOXharcosok.Size = new System.Drawing.Size(200, 292);
+            this.LBOXharcosok.TabIndex = 9;
+            // 
+            // LBOXkepessegek
+            // 
+            this.LBOXkepessegek.FormattingEnabled = true;
+            this.LBOXkepessegek.ItemHeight = 16;
+            this.LBOXkepessegek.Location = new System.Drawing.Point(228, 253);
+            this.LBOXkepessegek.Name = "LBOXkepessegek";
+            this.LBOXkepessegek.Size = new System.Drawing.Size(200, 292);
+            this.LBOXkepessegek.TabIndex = 10;
+            // 
+            // LBkepessegek
+            // 
+            this.LBkepessegek.AutoSize = true;
+            this.LBkepessegek.Location = new System.Drawing.Point(225, 232);
+            this.LBkepessegek.Name = "LBkepessegek";
+            this.LBkepessegek.Size = new System.Drawing.Size(86, 17);
+            this.LBkepessegek.TabIndex = 11;
+            this.LBkepessegek.Text = "Képességek";
+            // 
+            // BTNtöröl
+            // 
+            this.BTNtöröl.Location = new System.Drawing.Point(228, 551);
+            this.BTNtöröl.Name = "BTNtöröl";
+            this.BTNtöröl.Size = new System.Drawing.Size(105, 33);
+            this.BTNtöröl.TabIndex = 8;
+            this.BTNtöröl.Text = "Töröl";
+            this.BTNtöröl.UseVisualStyleBackColor = true;
+            // 
+            // TBleiras2
+            // 
+            this.TBleiras2.Location = new System.Drawing.Point(434, 253);
+            this.TBleiras2.Multiline = true;
+            this.TBleiras2.Name = "TBleiras2";
+            this.TBleiras2.Size = new System.Drawing.Size(336, 186);
+            this.TBleiras2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(431, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Képesség leírás";
+            // 
+            // BTNmodositas
+            // 
+            this.BTNmodositas.Location = new System.Drawing.Point(665, 445);
+            this.BTNmodositas.Name = "BTNmodositas";
+            this.BTNmodositas.Size = new System.Drawing.Size(105, 30);
+            this.BTNmodositas.TabIndex = 12;
+            this.BTNmodositas.Text = "Módosítás";
+            this.BTNmodositas.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 596);
+            this.Controls.Add(this.BTNmodositas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TBleiras2);
+            this.Controls.Add(this.BTNtöröl);
+            this.Controls.Add(this.LBkepessegek);
+            this.Controls.Add(this.LBOXkepessegek);
+            this.Controls.Add(this.LBOXharcosok);
+            this.Controls.Add(this.LBharcosok);
+            this.Controls.Add(this.GBkepessegHozzaadas);
+            this.Controls.Add(this.GBharcosLetrehozas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.GBharcosLetrehozas.ResumeLayout(false);
+            this.GBharcosLetrehozas.PerformLayout();
+            this.GBkepessegHozzaadas.ResumeLayout(false);
+            this.GBkepessegHozzaadas.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GBharcosLetrehozas;
+        private System.Windows.Forms.Button BTNharcosLetrehozas;
+        private System.Windows.Forms.TextBox TBharcosNev;
+        private System.Windows.Forms.Label LBnev;
+        private System.Windows.Forms.GroupBox GBkepessegHozzaadas;
+        private System.Windows.Forms.TextBox TBleiras;
+        private System.Windows.Forms.Label LBleiras;
+        private System.Windows.Forms.Label LBhasznalo;
+        private System.Windows.Forms.ComboBox CBhasznalo;
+        private System.Windows.Forms.Button BTNkepesseg;
+        private System.Windows.Forms.TextBox TBnev2;
+        private System.Windows.Forms.Label LBnev2;
+        private System.Windows.Forms.Label LBharcosok;
+        private System.Windows.Forms.ListBox LBOXharcosok;
+        private System.Windows.Forms.ListBox LBOXkepessegek;
+        private System.Windows.Forms.Label LBkepessegek;
+        private System.Windows.Forms.Button BTNtöröl;
+        private System.Windows.Forms.TextBox TBleiras2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BTNmodositas;
+    }
+}
+
